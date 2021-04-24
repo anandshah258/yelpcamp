221adc79b2c510593c4c32a59a5911f190075b11
Anand Shah <anandshah258@gmail.com>
2021-04-24 20:18:57 +0530

    ready to deploy

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { descriptors, places } = require('./seedHelpers')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+//'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -19,7 +20,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (i = 0; i < 300; i++) {
+    for (i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const c = new Campground({
@@ -27,16 +28,16 @@ const seedDB = async () => {
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente rem iure debitis at sit consequatur atque a. Et culpa, ipsam aspernatur temporibus laudantium tempora sequi qui fugit porro. Quibusdam, dicta!',
             price,
-            author: '6001e8d9e4bfd827eeeea78c',
+            author: '600e637e6ddc060015081d0a',
             images: [
+                {
+                    "url": "https://res.cloudinary.com/yelpcampimgs/image/upload/v1611079886/Yelpcamp/rlie4d3bksjzatnygpsv.jpg",
+                    "filename": "Yelpcamp/rf9sqipofpxsw3dqzwcj"
+                },
                 {
                     "url": "https://res.cloudinary.com/yelpcampimgs/image/upload/v1611079885/Yelpcamp/r9o8rrw25aq1hu3rjhch.jpg",
                     "filename": "Yelpcamp/rmfniicawwe8wobz7uen"
                 },
-                {
-                    "url": "https://res.cloudinary.com/yelpcampimgs/image/upload/v1611079886/Yelpcamp/rlie4d3bksjzatnygpsv.jpg",
-                    "filename": "Yelpcamp/rf9sqipofpxsw3dqzwcj"
-                }
             ],
             geometry: {
                 type: "Point",
